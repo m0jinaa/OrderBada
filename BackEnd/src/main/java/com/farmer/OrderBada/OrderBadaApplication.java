62d2d467adcd5e67d2b966beeac7f3447ac1b2db
m0_jinaa <m0_jinaa@naver.com>
2024-06-06 03:48:20 +0900

    :tada: 프로젝트 생성

---
+++ BackEnd/src/main/java/com/farmer/OrderBada/OrderBadaApplication.java
@@ -0,0 +1,13 @@
+package com.farmer.OrderBada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderBadaApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/farmer/OrderBada/OrderBadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmer.OrderBada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderBadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
